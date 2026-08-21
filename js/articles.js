e291cbffba38f77e9448f2dcbc85e7d5a04b74cd
asamerry <asamerry72@gmail.com>
2026-08-20 23:24:26 -0700

    added article page for figs; added css and js for articles

---
+++ js/articles.js
@@ -0,0 +1,78 @@
+//// Carousel Gallery ////
+const galleryImages = document.querySelectorAll(".gallery-image");
+const galleryDots = document.querySelectorAll(".gallery-dot");
+
+const previousButton = document.querySelector(".previous");
+const nextButton = document.querySelector(".next");
+
+let currentImage = 0;
+
+function showImage(index) {
+
+    galleryImages.forEach(image => {
+        image.classList.remove("active");
+    });
+
+    galleryDots.forEach(dot => {
+        dot.classList.remove("active");
+    });
+
+    galleryImages[index].classList.add("active");
+    galleryDots[index].classList.add("active");
+}
+
+nextButton.addEventListener("click", () => {
+
+    currentImage =
+        (currentImage + 1) % galleryImages.length;
+
+    showImage(currentImage);
+
+});
+
+previousButton.addEventListener("click", () => {
+
+    currentImage =
+        (currentImage - 1 + galleryImages.length)
+        % galleryImages.length;
+
+    showImage(currentImage);
+
+});
+
+galleryDots.forEach(dot => {
+
+    dot.addEventListener("click", () => {
+
+        currentImage = Number(dot.dataset.image);
+
+        showImage(currentImage);
+
+    });
+
+});
+
+//// Content Buttons and Tabs ////
+const tabButtons = document.querySelectorAll(".tab-button");
+const tabPanels = document.querySelectorAll(".tab-panel");
+
+tabButtons.forEach(button => {
+    button.addEventListener("click", () => {
+
+        const selectedTab = button.dataset.tab;
+
+        tabButtons.forEach(tab => {
+            tab.classList.remove("active");
+        });
+
+        tabPanels.forEach(panel => {
+            panel.classList.remove("active");
+        });
+
+        button.classList.add("active");
+
+        document
+            .getElementById(selectedTab)
+            .classList.add("active");
+    });
+});
